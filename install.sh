f4ba704b986bf3c0bb7ce9183ae1c76a047f5cbb
lucky <takuto.yoshikai@gmail.com>
2021-01-07 00:28:49 +0900

    pip req

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 cd `dirname $0`
 git clone https://github.com/TakutoYoshikai/trident
 git clone https://github.com/TakutoYoshikai/elemental-sight
+cd elemental-sight
+pip install -r requirements.txt
